011773b8629f1602d5e316b64831604cded71db7
FellipiBorges <fellipiborgesmc@gmail.com>
2023-03-22 19:34:06 -0300

    Converting Str Int

---
+++ main.py
@@ -0,0 +1,16 @@
+print("***********************")
+print("Bem vindo ao jogo da adivinhação!")
+print("***********************")
+
+numero_secreto = 42
+
+chute = input("Digite o seu numero: ")
+
+chute = int(chute)
+
+print("Você digitou " , chute)
+
+if(numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você erroooou!")
