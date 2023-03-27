60c81a8b3e657779f62e393e7185c3154aec1d7f
FellipiBorges <fellipiborgesmc@gmail.com>
2023-03-27 19:40:49 -0300

    Testing while functions

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ print("Bem vindo ao jogo da adivinhação!")
 print("***********************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu numero: ")
+while(rodada <= total_de_tentativas):
+    print("Tentativas", rodada, "de", total_de_tentativas)
+    chute = input("Digite o seu numero: ")
+    chute = int(chute)
+    print("Você digitou " , chute)
 
-chute = int(chute)
+    acertou =   chute == numero_secreto
+    maior =     chute > numero_secreto
+    menor =     chute < numero_secreto
 
-print("Você digitou " , chute)
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você erroooou! Seu chute foi maior que o número secreto")
+        if(menor):
+            print("Você erroooou! Seu chute foi menor que o número secreto")
 
-acertou =   chute == numero_secreto
-maior =     chute > numero_secreto
-menor =     chute < numero_secreto
-
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você erroooou! Seu chute foi maior que o número secreto")
-    if(menor):
-        print("Você erroooou! Seu chute foi menor que o número secreto")
+        rodada = rodada + 1
 
 print("Fim de Jogo")
